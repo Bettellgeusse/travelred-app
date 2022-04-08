1742e9412035cacfed556566730fa8a01af5e64f
Juan Mora <jpmoratres@gmail.com>
2022-04-08 15:25:05 -0500

    prueba 5 heroku sin BD

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../lib/auth');
 
-router.get('/prueba',  (req, res) => {
+router.get('/',  (req, res) => {
    res.send('Hola maricas');
 });
 
