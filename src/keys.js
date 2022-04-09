6fab97b59a84424ebe3e1a2e0d40af83735ee87b
Juan Mora <jpmoratres@gmail.com>
2022-04-08 22:36:35 -0500

    claves servidor heroku

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
  
    database: {
-       host: 'localhost',
-       user: 'root',
-       password: '',
-       database: 'travel'
+       host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+       user: 'g2nhc8iwz4df8x9l',
+       password: 'iq1fr0ldrmozk3yo',
+       database: 'efvyxxt5qnvu542e'
    }
 
 };
