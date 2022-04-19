0fbe85cd3b148b82cb96d61dc528eca30dda8b66
Juan Mora <jpmoratres@gmail.com>
2022-04-19 17:05:18 -0500

    query reservas ID

--- src/keys.js
+++ src/keys.js
@@ -1,19 +1,19 @@
 module.exports = {
  
-   database: {
-       host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-       user: 'g2nhc8iwz4df8x9l',
-       password: 'iq1fr0ldrmozk3yo',
-       database: 'efvyxxt5qnvu542e'
-   }
+//    database: {
+//        host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//        user: 'g2nhc8iwz4df8x9l',
+//        password: 'iq1fr0ldrmozk3yo',
+//        database: 'efvyxxt5qnvu542e'
+//    }
 
    //local
    
-   // database: {
-   //  host: 'localhost',
-   //  user: 'root',
-   //  password: '',
-   //  database: 'travel'
-   // } 
+   database: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'travel'
+   } 
 
 };
