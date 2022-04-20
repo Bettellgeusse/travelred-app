9d17c96fa02ed315786f5aaacc5693fb9b26e9e6
Juan Mora <jpmoratres@gmail.com>
2022-04-20 11:53:52 -0500

    query grupos

--- src/controller/grupos.js
+++ src/controller/grupos.js
@@ -20,14 +20,14 @@ const poolGrupos = async (req, res) => {
 }
 
 const getGrupos = async (req, res) => {
-    const vergrupos = await pool.query('SELECT * FROM grupo');
+    const vergrupos = await pool.query('SELECT g.GRP_ID,g.GRP_VALORTOTAL,g.GRP_VALORSALDO,g.GRP_OBSERVACION,g.CLN_ID,c.CLN_NOMBRE,c.CLN_CEDULA,c.CLN_CELULAR FROM grupo AS g INNER JOIN cliente AS c ON g.CLN_ID = c.CLN_ID;');
     //res.render('grupos/listar_grupos', {vergrupos:vergrupos});
     return res.json(vergrupos);
 }
 
 const getIdGrupos = async (req, res) => {
     const {id} = req.params;
-    const listarGrupos = await pool.query('SELECT * FROM cuentas WHERE GRP_ID = ?',[id]);
+    const listarGrupos = await pool.query('SELECT g.GRP_ID,g.GRP_VALORTOTAL,g.GRP_VALORSALDO,g.GRP_OBSERVACION,g.CLN_ID,c.CLN_NOMBRE,c.CLN_CEDULA,c.CLN_CELULAR FROM grupo AS g INNER JOIN cliente AS c ON g.CLN_ID = c.CLN_ID; WHERE GRP_ID = ?',[id]);
     console.log(listarGrupos[0]);
     return res.json(listarGrupos[0])
 }
