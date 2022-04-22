161a5320079af8844b8b35ea9ddd5fa6873c08e1
Juan Mora <jpmoratres@gmail.com>
2022-04-22 01:16:48 -0500

    login true dreamhost

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
      //   sameSite: "none",
-        secure: false,
+        secure: true,
      //   maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
       },
     store: new MySQLStore(database)
