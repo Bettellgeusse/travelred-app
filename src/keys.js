f78214df0995c4870783b1d045e4dd47e5ef2cd1
Juan Mora <jpmoratres@gmail.com>
2022-07-12 14:06:00 -0500

    base de datos heroku

--- src/keys.js
+++ src/keys.js
@@ -1,21 +1,21 @@
 module.exports = {
  
-   // database: {
-   //     host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-   //     user: 'g2nhc8iwz4df8x9l',
-   //     password: 'iq1fr0ldrmozk3yo',
-   //     database: 'efvyxxt5qnvu542e'
-   // }
+   database: {
+       host: 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+       user: 'g2nhc8iwz4df8x9l',
+       password: 'iq1fr0ldrmozk3yo',
+       database: 'efvyxxt5qnvu542e'
+   }
 
 
 // Dream
 
-database: {
-   host: 'travel.app.travelred.com.co',
-   user: 'apptravel',
-   password: 'aLpxZGLGMAD92L6',
-   database: 'apptravelred'
-  } 
+// database: {
+//    host: 'travel.app.travelred.com.co',
+//    user: 'apptravel',
+//    password: 'aLpxZGLGMAD92L6',
+//    database: 'apptravelred'
+//   } 
 
 
 
